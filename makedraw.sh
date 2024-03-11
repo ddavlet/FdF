a85fd01e48d90065f7065c107ab2b5f9a01fec1d
Dilshod Davletyarov <dilshod95@gmail.com>
2024-03-11 09:04:30 +0100

    makefile

---
+++ makedraw.sh
@@ -0,0 +1,216 @@
+#!/bin/bash
+echo ""
+sleep 0
+echo ""
+sleep 0
+echo ""
+sleep 0
+echo ""
+sleep 0
+echo ""
+sleep 0
+echo "                          /\\      "
+sleep 0.05
+echo "                         /  \\    "
+sleep 0.05
+echo "                        /    \\   "
+sleep 0.05
+echo "                       /      \\  "
+sleep 0.05
+echo "                      /        \\ "
+sleep 0.05
+echo "                     |          |"
+sleep 0.05
+echo "                     |          |"
+sleep 0.05
+echo "                     |   ___    |"
+sleep 0.05
+echo "                     |  /   \\   |"
+sleep 0.05
+echo "                     | |     |  |"
+sleep 0.05
+echo "                     |  \\___/   |                  :::      ::::::::   "
+sleep 0.05
+echo "                    /|          |\                :+:      :+:    :+:   "
+sleep 0.05
+echo "                   / |          | \             +:+ +:+         +:+     "
+sleep 0.05
+echo "                  /  |          |  \          +#+  +:+       +#+        "
+sleep 0.05
+echo "                 /___|          |___\       +#+#+#+#+#+   +#+           "
+sleep 0.05
+echo "                     |          |                #+#    #+#             "
+sleep 0.05
+echo "                     |          |               ###   ########.fr       "
+sleep 0.05
+echo "                     |          |"
+sleep 0.05
+echo "                     |          |"
+sleep 0.05
+echo "                     |          |"
+sleep 0.05
+echo "                     |          |"
+sleep 0.05
+echo "                     |          |"
+sleep 0.05
+echo "                    //          \\\\"
+sleep 0.05
+echo "                   // /        \\ \\\\"
+sleep 0.05
+echo "                  // / |      | \\ \\\\"
+sleep 0.05
+echo "                 // /  |      |  \\ \\\\"
+sleep 0.05
+echo "                // / / |      | \\ \\ \\\\"
+sleep 0.05
+echo "               // / /  |      |  \\ \\ \\\\"
+sleep 0.05
+echo "              //_/_/_/_|      |_\\_\\_\\_\\\\"
+sleep 0.05
+echo ""
+sleep 0.05
+echo "                  //////|||||\\\\\\\\\\\\    "
+sleep 0.05
+echo "                 ///////|||||\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "                ////////|||||\\\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "               ////////|||||||\\\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "              ////////|||||||||\\\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "             ////////|||||||||||\\\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "            ////////|||||||||||||\\\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "           ////////|||||||||||||||\\\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "          ////////|||||||||||||||||\\\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "         ///////|||||||||||||||||||||\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "        ///////|||||||||||||||||||||||\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "       ///////|||||||||||||||||||||||||\\\\\\\\\\\\\\   "
+sleep 0.05
+echo "       //////|||||||||||||||||||||||||||\\\\\\\\\\\\   "
+sleep 0.05
+echo "       /////|||||||||||||||||||||||||||||\\\\\\\\\\   "
+sleep 0.05
+echo "       ////|||||||||||||||||||||||||||||||\\\\\\\\   "
+sleep 0.05
+echo "        //|||||||||||||||||||||||||||||||||\\\\   "
+sleep 0.05
+echo "          |||||||||||||||||||||||||||||||||   "
+sleep 0.05
+echo "           |||||||||||||||||||||||||||||||   "
+sleep 0.05
+echo "            |||||||||||||||||||||||||||||   "
+sleep 0.05
+echo "             |||||||||||||||||||||||||||   "
+sleep 0.05
+echo "              |||||||||||||||||||||||||   "
+sleep 0.05
+echo "               |||||||||||||||||||||||   "
+sleep 0.05
+echo "                |||||||||||||||||||||  "
+sleep 0.05
+echo "                 |||||||||||||||||||  "
+sleep 0.05
+echo "                  |||||||||||||||||  "
+sleep 0.05
+echo "                   |||||||||||||||  "
+sleep 0.05
+echo "                    |||||||||||||  "
+sleep 0.05
+echo "                     |||||||||||  "
+sleep 0.05
+echo "                      |||||||||  "
+sleep 0.05
+echo "                       |||||||  "
+sleep 0.05
+echo "                        |||||  "
+sleep 0.05
+echo "                         |||  "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+sleep 0.05
+echo "                            "
+
+
